266436e426cfd2e6826e346ea3341b9bcab948b9
Fantasmartu <fantasmartu@gmail.com>
2024-02-14 17:35:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const yesBtn = document.querySelector('#yesBtn');
 yesBtn.addEventListener('click',function () {
     alert('Pues a jugar :)')
     window.open("https://app.mediafire.com/myfiles")
+    document.body.innerHTML = `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./css/board.css">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>Memorama</title>
+    </head>
+    <body>
+
+        <h1>A jugar!!! 😎</h1>
+
+        <main class="board-game">
+            <!-- Agrega aquí las figuras y cartas -->
+        </main>
+
+        <script src="index.js"></script>
+    </body>
+    </html>
+`;
 });
 const noBtn =  document.querySelector('#noBtn')
 
